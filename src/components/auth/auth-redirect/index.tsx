5b3208e08b724071dbd85562903951742cd422b6
Nguyễn Quốc Chung <quocchung.starack@gmail.com>
2024-08-30 18:29:59 +0700

    feat: end project

--- src/components/auth/auth-redirect/index.tsx
+++ src/components/auth/auth-redirect/index.tsx
@@ -5,6 +5,7 @@ import { useNavigate } from 'react-router-dom';
 import { request } from '../../../api/request';
 import { useDispatch } from 'react-redux';
 import { UserActions } from '../../../redux/user';
+import { SnackbarActions } from '../../../redux/snackbar';
 
 const useAuthRedirect = () => {
     const navigate = useNavigate();
@@ -13,11 +14,12 @@ const useAuthRedirect = () => {
         const checkTokens = async () => {
             const accessToken = Cookies.get('accessToken');
             const refreshToken = Cookies.get('refreshToken');
-            
+
             if (accessToken) {
                 try {
-                    const decoded: any = jwtDecode(accessToken);                    
-                    const now = Math.floor(Date.now() / 1000); 
+                    const decoded: any = jwtDecode(accessToken);
+                    const now = Math.floor(Date.now() / 1000);
+                    console.log("accessToken", decoded, decoded.exp, now);
                     if (decoded.exp && decoded.exp > now) {
                         const data = await request("GET", "", `users/${decoded.sub}`)
                         dispatch(UserActions.setUser(data))
@@ -31,10 +33,23 @@ const useAuthRedirect = () => {
 
             if (refreshToken) {
                 try {
-                    const response = await request("POST", {refreshToken: refreshToken} , "auth/refresh-token")
-                    if (response.status == 201) {
-                        Cookies.set('accessToken', response.data, { expires: 60, path: '/' }); 
-                        navigate('/');
+                    const decoded: any = jwtDecode(refreshToken);
+                    const now = Math.floor(Date.now() / 1000);
+                    console.log("refreshToken", decoded, decoded.exp, now);
+                    if (decoded.exp && decoded.exp > now) {
+                        const response = await request("POST", { refreshToken: refreshToken }, "auth/refresh-token")
+                        if (response.status == 201) {
+                            Cookies.set('accessToken', response.data, { expires: 60, path: '/' });
+                            navigate('/');
+                        } else {
+                            dispatch(SnackbarActions.OpenSnackbar(
+                                {
+                                    open: true,
+                                    content: "Login session has expired.",
+                                    state: "warn",
+                                }))
+                            navigate('/auth?page=signin');
+                        }
                     } else {
                         navigate('/auth?page=signin');
                     }
--- src/components/home/header/index.tsx
+++ src/components/home/header/index.tsx
@@ -63,150 +63,148 @@ export default function Header() {
     const location = useLocation();
     return (
         <Box>
-            {
-                user && <Box
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    position: 'fixed',
+                    top: '0',
+                    left: '0',
+                    width: '100%',
+                    zIndex: 100000
+                }}
+            >
+                <Box
                     sx={{
+                        width: '-webkit-fill-available',
+                        padding: '10px 10%',
+                        height: '60px',
+                        backgroundColor: 'white',
                         display: 'flex',
-                        flexDirection: 'column',
-                        position: 'fixed',
-                        top: '0',
-                        left: '0',
-                        width: '100%',
-                        zIndex: 100000
+                        alignItems: 'center',
+                        justifyContent: 'space-between',
                     }}
                 >
                     <Box
                         sx={{
-                            width: '-webkit-fill-available',
-                            padding: '10px 10%',
-                            height: '60px',
-                            backgroundColor: 'white',
-                            display: 'flex',
-                            alignItems: 'center',
-                            justifyContent: 'space-between',
+                            height: '100%'
                         }}
                     >
-                        <Box
-                            sx={{
-                                height: '100%'
+                        <img src="/Images/auth/logo.png"
+                            style={{
+                                height: '100%',
                             }}
-                        >
-                            <img src="/Images/auth/logo.png"
-                                style={{
-                                    height: '100%',
-                                }}
-                            />
-                        </Box>
-                        <Box
-                            sx={{
-                                height: '80%',
-                                display: 'flex',
-                                gap: '20px'
-                            }}
-                        >
-                            {
-                                contact.map((item: any, index: number) => (
-                                    <Box
-                                        sx={{
-                                            display: 'flex',
-                                            gap: '10px',
-                                            alignItems: 'center'
-                                        }}
-                                    >
-                                        <img src={item.icon}
-                                            style={{
-                                                height: '35px'
-                                            }}
-                                        />
-                                        <Box>
-                                            <p
-                                                style={{
-                                                    color: '#1F2B6C',
-                                                    fontSize: '16px',
-                                                    fontFamily: 'Work Sans, sans-serif'
-                                                }}
-                                            >{item.name}</p>
-                                            <p style={{
-                                                color: '#159EEC',
-                                                fontSize: '16px',
-                                                fontFamily: 'Work Sans, sans-serif'
-                                            }}>{item.content}</p>
-                                        </Box>
-                                    </Box>
-                                ))
-                            }
-                        </Box>
+                        />
                     </Box>
                     <Box
                         sx={{
-                            width: '-webkit-fill-available',
-                            height: '60px',
-                            backgroundColor: '#1F2B6C',
+                            height: '80%',
                             display: 'flex',
-                            alignItems: 'center',
-                            justifyContent: 'space-between',
-                            padding: '10px 10%'
+                            gap: '20px'
                         }}
                     >
-                        <Box
-                            sx={{
-                                display: 'flex',
-                                gap: '30px'
-                            }}
-                        >
-                            {
-                                menu.map((item: any, index: number) => (
-                                    <a href={item.link}
+                        {
+                            contact.map((item: any, index: number) => (
+                                <Box
+                                    sx={{
+                                        display: 'flex',
+                                        gap: '10px',
+                                        alignItems: 'center'
+                                    }}
+                                >
+                                    <img src={item.icon}
                                         style={{
-                                            color: location.pathname == item.link ? '#BFD2F8' : 'white',
-                                            fontSize: '18px',
-                                            fontWeight: location.pathname == item.link ? 'bold' : 'inherit',
-                                            fontFamily: 'Work Sans, sans-serif',
-                                            textDecoration: 'none'
+                                            height: '35px'
                                         }}
-                                    >
-                                        {item.name}
-                                    </a>
-                                ))
-                            }
-                        </Box>
-                        <Box
-                            sx={{
-                                display: 'flex',
-                                gap: '30px',
-                                alignItems: 'center'
-                            }}
-                        >
-                            <img src="/Images/home/header/search.svg" />
-                            <Button variant="contained"
-                                sx={{
-                                    borderRadius: '50px',
-                                    backgroundColor: '#BFD2F8',
-                                    width: '170px',
-                                    color: '#1F2B6C',
-                                    fontSize: '16px',
-                                    fontFamily: 'Work Sans, sans-serif'
-                                }}
-                            >
-                                Appointment
-                            </Button>
-                            <Button variant="contained"
-                                sx={{
-                                    borderRadius: '50px',
-                                    backgroundColor: '#BFD2F8',
-                                    width: '170px',
-                                    color: '#1F2B6C',
-                                    fontSize: '16px',
-                                    fontFamily: 'Work Sans, sans-serif'
-                                }}
-                                onClick={user ? handleLogout : handleLogin}
-                            >
-                                {user ? "Logout" : "Login"}
-                            </Button>
-                        </Box>
+                                    />
+                                    <Box>
+                                        <p
+                                            style={{
+                                                color: '#1F2B6C',
+                                                fontSize: '16px',
+                                                fontFamily: 'Work Sans, sans-serif'
+                                            }}
+                                        >{item.name}</p>
+                                        <p style={{
+                                            color: '#159EEC',
+                                            fontSize: '16px',
+                                            fontFamily: 'Work Sans, sans-serif'
+                                        }}>{item.content}</p>
+                                    </Box>
+                                </Box>
+                            ))
+                        }
                     </Box>
                 </Box>
-            }
+                <Box
+                    sx={{
+                        width: '-webkit-fill-available',
+                        height: '60px',
+                        backgroundColor: '#1F2B6C',
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'space-between',
+                        padding: '10px 10%'
+                    }}
+                >
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            gap: '30px'
+                        }}
+                    >
+                        {
+                            menu.map((item: any, index: number) => (
+                                <a href={item.link}
+                                    style={{
+                                        color: location.pathname == item.link ? '#BFD2F8' : 'white',
+                                        fontSize: '18px',
+                                        fontWeight: location.pathname == item.link ? 'bold' : 'inherit',
+                                        fontFamily: 'Work Sans, sans-serif',
+                                        textDecoration: 'none'
+                                    }}
+                                >
+                                    {item.name}
+                                </a>
+                            ))
+                        }
+                    </Box>
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            gap: '30px',
+                            alignItems: 'center'
+                        }}
+                    >
+                        <img src="/Images/home/header/search.svg" />
+                        <Button variant="contained"
+                            sx={{
+                                borderRadius: '50px',
+                                backgroundColor: '#BFD2F8',
+                                width: '170px',
+                                color: '#1F2B6C',
+                                fontSize: '16px',
+                                fontFamily: 'Work Sans, sans-serif'
+                            }}
+                        >
+                            Appointment
+                        </Button>
+                        <Button variant="contained"
+                            sx={{
+                                borderRadius: '50px',
+                                backgroundColor: '#BFD2F8',
+                                width: '170px',
+                                color: '#1F2B6C',
+                                fontSize: '16px',
+                                fontFamily: 'Work Sans, sans-serif'
+                            }}
+                            onClick={user ? handleLogout : handleLogin}
+                        >
+                            {user ? "Logout" : "Login"}
+                        </Button>
+                    </Box>
+                </Box>
+            </Box>
         </Box>
 
     );
