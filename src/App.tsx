ab14d22018c30df911615527c3b8b7dbd9863054
Liorion Nguyen <liorion.nguyen@gmail.com>
2024-09-24 07:39:56 +0700

    first: commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { useEffect } from "react";
-import Home from './page/home';
 import Auth from './page/auth';
 import Admin from './page/admin';
 import useAuthRedirect from './components/auth/auth-redirect';
@@ -13,13 +12,6 @@ function App() {
     <Router>
       <AuthWrapper /> 
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<Home />} />
-        <Route path="/services" element={<Home />} />
-        <Route path="/doctors" element={<Home />} />
-        <Route path="/news" element={<Home />} />
-        <Route path="/contact" element={<Home />} />
-        <Route path="/advise" element={<Home />} />
         <Route path="/auth" element={<Auth />} />
         <Route path="/admin" element={<Admin />} />
         <Route path="/admin/:link" element={<Admin />} />
